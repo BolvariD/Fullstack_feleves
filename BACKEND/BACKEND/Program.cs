3bb2f915b824db44a4d0b774b807ab219774f7b0
bolvarid <bolvarid@gmail.com>
2025-05-10 23:10:23 +0200

    added swagger

--- BACKEND/BACKEND/Program.cs
+++ BACKEND/BACKEND/Program.cs
@@ -1,6 +1,18 @@
+using BACKEND.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ILoanRepository, LoanRepository>();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
