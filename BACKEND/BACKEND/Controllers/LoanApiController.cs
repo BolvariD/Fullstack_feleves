fe2256ab1162c3712dc5bef385d4fd5419e343e2
bolvarid <bolvarid@gmail.com>
2025-05-10 23:01:47 +0200

    Controller untested, moving on

---
+++ BACKEND/BACKEND/Controllers/LoanApiController.cs
@@ -0,0 +1,30 @@
+﻿using BACKEND.Data;
+using BACKEND.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanApiController : Controller
+    {
+        ILoanRepository repo;
+
+        public LoanApiController(ILoanRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Loan> GetLoans()
+        {
+            return repo.GetAll();
+        }
+
+        [HttpPost]
+        public void PostLoans(int amount, double interestRate)
+        {
+            repo.AddLoan(amount, interestRate);
+        }
+    }
+}
