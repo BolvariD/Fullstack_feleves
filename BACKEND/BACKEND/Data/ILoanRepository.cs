a9a10f4a018f1892f677ddfa32386bb08ad6ad72
bolvarid <bolvarid@gmail.com>
2025-05-10 22:56:14 +0200

    Loan repo interface done

---
+++ BACKEND/BACKEND/Data/ILoanRepository.cs
@@ -0,0 +1,10 @@
+﻿using BACKEND.Model;
+
+namespace BACKEND.Data
+{
+    public interface ILoanRepository
+    {
+        public void AddLoan(int amount, double interestRate);
+        public IEnumerable<Loan> GetAll();
+    }
+}
